68dcb9e2619194c9644def5e50ea79a0cd45f7dd
Illya Viktorovich <trismetry@gmail.com>
2024-09-30 21:39:04 +0300

    11

--- WebApplication2/WeatherForecast.cs
+++ WebApplication2/WeatherForecast.cs
@@ -5,7 +5,7 @@ namespace WebApplication2
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
-
+        dtr
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
